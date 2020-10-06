713e970469ce17609aea38f7e7383c1e8780e4fd
vonuki <vonuki@gmail.com>
2020-10-06 12:06:41 -0400

    initial (configured Advanced Template)

---
+++ common/config/params.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'adminEmail' => 'info@easymatic.su',
+    'supportEmail' => 'robot@easymatic.su',
+    'senderEmail' => 'robot@easymatic.su',
+    'senderName' => 'Example.com mailer',
+    'user.passwordResetTokenExpire' => 3600,
+    'user.passwordMinLength' => 8,
+];
